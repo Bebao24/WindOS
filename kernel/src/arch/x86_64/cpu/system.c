ad876099a5a6d939ac5ce136353890f67e3abaca
Bebao24 <baong2408@gmail.com>
2025-01-05 11:32:37 +0700

    Implemented outb and inb

--- kernel/src/arch/x86_64/cpu/system.c
+++ kernel/src/arch/x86_64/cpu/system.c
@@ -5,3 +5,15 @@ void panic()
     asm volatile ("cli; hlt");
 }
 
+void x64_outb(uint16_t port, uint8_t value)
+{
+    asm volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
+uint8_t x64_inb(uint16_t port)
+{
+    uint8_t returnValue;
+    asm volatile ("inb %1, %0" : "=a"(returnValue) : "Nd"(port));
+    return returnValue;
+}
+
--- kernel/src/arch/x86_64/cpu/system.h
+++ kernel/src/arch/x86_64/cpu/system.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <stdint.h>
 
 void panic();
+
+void x64_outb(uint16_t port, uint8_t value);
+uint8_t x64_inb(uint16_t port);
 
