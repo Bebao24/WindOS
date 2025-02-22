ee5bc150ab1cdffb69a50d6bf249d5d34844feee
Bebao24 <baong2408@gmail.com>
2025-02-22 11:32:34 +0700

    Added the TSS in the GDT

--- kernel/src/arch/x86_64/cpu/gdt.c
+++ kernel/src/arch/x86_64/cpu/gdt.c
@@ -1,13 +1,33 @@
 #include "gdt.h"
 #include <stdint.h>
+#include <memory.h>
 
 uint64_t gdt_entries[5];
 
 static GDTEntries gdt;
 static GDTR gdtr;
 
+static TSSPtr tss;
+
+TSSPtr* tssPtr = &tss;
+
 // From gdt.asm
 extern void LoadGDT(GDTR* gdtr);
+
+void GDT_LoadTSS(TSSPtr* tss)
+{
+	uint64_t TSSAddr = (uint64_t)tss;
+
+	gdt.tss.BaseLow = (uint16_t)TSSAddr;
+	gdt.tss.BaseMid = (uint8_t)(TSSAddr >> 16);
+	gdt.tss.Flags1 = 0b10001001;
+	gdt.tss.Flags2 = 0;
+	gdt.tss.BaseHigh = (uint8_t)(TSSAddr >> 24);
+	gdt.tss.BaseUpper = (uint32_t)(TSSAddr >> 32);
+	gdt.tss.Reserved = 0;
+
+	asm volatile("ltr %0" : : "rm"((uint16_t)GDT_TSS) : "memory");
+}
 
 void InitializeGDT()
 {
@@ -35,10 +55,23 @@ void InitializeGDT()
 	gdt.descriptors[2].Granularity = 0;
 	gdt.descriptors[2].BaseHigh = 0;
 
+	// GDT TSS
+	gdt.tss.Limit = 104;
+	gdt.tss.BaseLow = 0;
+	gdt.tss.BaseMid = 0;
+	gdt.tss.Flags1 = 0b10001001;
+	gdt.tss.Flags2 = 0;
+	gdt.tss.BaseHigh = 0;
+	gdt.tss.BaseUpper = 0;
+	gdt.tss.Reserved = 0;
+
 	gdtr.Limit = sizeof(GDTEntries) - 1;
 	gdtr.Base = (uint64_t)&gdt;
 
 	LoadGDT(&gdtr);
 
+	// Prepare for the TSS
+	memset(&tss, 0, sizeof(TSSPtr));
+	GDT_LoadTSS(&tss);
 }
 
--- kernel/src/arch/x86_64/cpu/gdt.h
+++ kernel/src/arch/x86_64/cpu/gdt.h
@@ -3,6 +3,7 @@
 
 #define GDT_KERNEL_CODE 0x08
 #define GDT_KERNEL_DATA 0x10
+#define GDT_TSS 0x18
 
 typedef struct 
 {
@@ -22,7 +23,40 @@ typedef struct
 
 typedef struct
 {
+    uint16_t Limit;
+    uint16_t BaseLow;
+    uint8_t BaseMid;
+    uint8_t Flags1;
+    uint8_t Flags2;
+    uint8_t BaseHigh;
+    uint32_t BaseUpper;
+    uint32_t Reserved;
+} __attribute__((packed)) TSSEntry;
+
+typedef struct tss
+{
+    uint32_t reserved0;
+    uint64_t rsp0;
+    uint64_t rsp1;
+    uint64_t rsp2;
+    uint64_t reserved1;
+    uint64_t reserved2;
+    uint64_t ist1;
+    uint64_t ist2;
+    uint64_t ist3;
+    uint64_t ist4;
+    uint64_t ist5;
+    uint64_t ist6;
+    uint64_t ist7;
+    uint64_t reserved3;
+    uint16_t reserved4;
+    uint16_t io_bitmap_offset;
+}__attribute__((__packed__)) TSSPtr;
+
+typedef struct
+{
     GDTEntry descriptors[3];
+    TSSEntry tss;
 } __attribute__((packed)) GDTEntries;
 
 
