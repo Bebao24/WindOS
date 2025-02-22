07b29cb54a2ca81d93559ae9d2c1b7d7aa747472
Bebao24 <baong2408@gmail.com>
2025-02-22 11:14:17 +0700

    Rewrite the whole GDT implementation

--- kernel/src/arch/x86_64/cpu/gdt.c
+++ kernel/src/arch/x86_64/cpu/gdt.c
@@ -1,55 +1,44 @@
 #include "gdt.h"
 #include <stdint.h>
 
-typedef struct
-{
-    uint16_t limit;
-    uint64_t base;
-} __attribute__((packed)) GDTR;
-
 uint64_t gdt_entries[5];
 
-GDTR default_gdtr;
+static GDTEntries gdt;
+static GDTR gdtr;
 
 // From gdt.asm
 extern void LoadGDT(GDTR* gdtr);
 
 void InitializeGDT()
 {
-    // NULL descriptor
-    gdt_entries[0] = 0;
+	// NULL descriptor
+	gdt.descriptors[0].Limit = 0;
+	gdt.descriptors[0].BaseLow = 0;
+	gdt.descriptors[0].BaseMid = 0;
+	gdt.descriptors[0].Access = 0;
+	gdt.descriptors[0].Granularity = 0;
+	gdt.descriptors[0].BaseHigh = 0;
 
-    // Kernel code
-    uint64_t kernel_code = 0;
-    kernel_code |= 0b1011 << 8; // Type of selector
-    kernel_code |= 1 << 12; // Not a system descriptor
-    kernel_code |= 0 << 13; // Ring 0
-    kernel_code |= 1 << 15; // Present
-    kernel_code |= 1 << 21; // Long mode segment
-    gdt_entries[1] = kernel_code << 32;
+	// Kernel code
+	gdt.descriptors[1].Limit = 0;
+	gdt.descriptors[1].BaseLow = 0;
+	gdt.descriptors[1].BaseMid = 0;
+	gdt.descriptors[1].Access = 0b10011010;
+	gdt.descriptors[1].Granularity = 0b00100000;
+	gdt.descriptors[1].BaseHigh = 0;
 
-    // Kernel data
-    uint64_t kernel_data = 0;
-    kernel_data |= 0b0011 << 8; // Type of selector
-    kernel_data |= 1 << 12; // Not a system descriptor
-    kernel_data |= 0 << 13; // Ring 0
-    kernel_data |= 1 << 15; // Present
-    kernel_data |= 1 << 21; // Long mode segment
-    gdt_entries[2] = kernel_data << 32;
+	// Kernel data
+	gdt.descriptors[2].Limit = 0;
+	gdt.descriptors[2].BaseLow = 0;
+	gdt.descriptors[2].BaseMid = 0;
+	gdt.descriptors[2].Access = 0b10010010;
+	gdt.descriptors[2].Granularity = 0;
+	gdt.descriptors[2].BaseHigh = 0;
 
-    // User code
-    uint64_t user_code = kernel_code | (3 << 13); // Ring 3
-    gdt_entries[3] = user_code;
+	gdtr.Limit = sizeof(GDTEntries) - 1;
+	gdtr.Base = (uint64_t)&gdt;
 
-    // User data
-    uint64_t user_data = kernel_data | (3 << 13); // Ring 3
-    gdt_entries[4] = user_data;
+	LoadGDT(&gdtr);
 
-    // Setup the GDTR
-    default_gdtr.limit = sizeof(gdt_entries) - 1;
-    default_gdtr.base = (uint64_t)gdt_entries;
-
-    // Load and flush the GDT
-    LoadGDT(&default_gdtr);
 }
 
--- kernel/src/arch/x86_64/cpu/gdt.h
+++ kernel/src/arch/x86_64/cpu/gdt.h
@@ -1,4 +1,30 @@
 #pragma once
+#include <stdint.h>
+
+#define GDT_KERNEL_CODE 0x08
+#define GDT_KERNEL_DATA 0x10
+
+typedef struct 
+{
+    uint16_t Limit;
+    uint16_t BaseLow;
+    uint8_t BaseMid;
+    uint8_t Access;
+    uint8_t Granularity;
+    uint8_t BaseHigh;
+} __attribute__((packed)) GDTEntry;
+
+typedef struct
+{
+    uint16_t Limit; // Basically the size
+    uint64_t Base;
+} __attribute__((packed)) GDTR;
+
+typedef struct
+{
+    GDTEntry descriptors[3];
+} __attribute__((packed)) GDTEntries;
+
 
 void InitializeGDT();
 
